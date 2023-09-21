e0e76dbe5d42babd8fd5dc5157a68180ac2c2b81
Repnikov.A <repnikov.a@florcom.ru>
2023-09-21 14:16:13 +0300

    Добавьте файлы проекта.

---
+++ Modul.14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+        }
+
+
+        public class City
+        {
+            public City(string name, long population)
+            {
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
+        }
+    }
+}
