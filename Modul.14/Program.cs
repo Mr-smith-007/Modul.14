f92aceafa0d7575d1273b8e7b97f5afe02d14370
Repnikov.A <repnikov.a@florcom.ru>
2023-09-21 15:58:04 +0300

    14.2.2

--- Modul.14/Program.cs
+++ Modul.14/Program.cs
@@ -10,20 +10,14 @@ namespace LinqTest
         static void Main(string[] args)
         {
 
-            var numsList = new List<int[]>()
+            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+
+            var orderedWords = words.OrderBy(x => x.Length).Select(x => new {Word = x, Num = x.Length});
+
+            foreach (var word in orderedWords)
             {
-
-                new[] {2, 3, 7, 1},
-                new[] {45, 17, 88, 0},
-                new[] {23, 32, 44, -6},
-
-            };
-
-            var orderByList = numsList.SelectMany(s=>s).OrderBy(s => s);
-
-            foreach (var num in orderByList) 
-            Console.WriteLine(num);
-
+                Console.WriteLine($"В слове {word.Word} - {word.Num} букв");
+            }
         }
 
 
