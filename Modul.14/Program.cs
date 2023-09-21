bcf87bab4376863a09d1fa39721d82388c9e641f
Repnikov.A <repnikov.a@florcom.ru>
2023-09-21 14:42:40 +0300

    14.1.2

--- Modul.14/Program.cs
+++ Modul.14/Program.cs
@@ -9,32 +9,19 @@ namespace LinqTest
 
         static void Main(string[] args)
         {
+
+            string[] text = { "Раз два три", "четыре пять шесть", "семь восемь девять" };
+            var words = from str in text 
+                        from word in str.Split(' ') 
+                        select word; 
+
             
-            var russianCities = new List<City>();
-            russianCities.Add(new City("Москва", 11900000));
-            russianCities.Add(new City("Санкт-Петербург", 4991000));
-            russianCities.Add(new City("Волгоград", 1099000));
-            russianCities.Add(new City("Казань", 1169000));
-            russianCities.Add(new City("Севастополь", 449138));
+            foreach (var word in words)
+                Console.WriteLine(word);
 
-            var nameCities = russianCities.Where(c => c.Name.Length <= 10).OrderBy(c => c.Name.Length);
-            foreach (var c in nameCities)
-            {
-                Console.WriteLine($"Город {c.Name} имеет в названии {c.Name.Length} букв");
-            }
+
         }
 
 
-        public class City
-        {
-            public City(string name, long population)
-            {
-                Name = name;
-                Population = population;
-            }
-
-            public string Name { get; set; }
-            public long Population { get; set; }
-        }
     }
 }
