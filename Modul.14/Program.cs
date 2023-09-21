afaa4ea6d49a3c2214527b5b824452e1d90109db
Repnikov.A <repnikov.a@florcom.ru>
2023-09-21 16:23:26 +0300

    14.2.3

--- Modul.14/Program.cs
+++ Modul.14/Program.cs
@@ -10,16 +10,40 @@ namespace LinqTest
         static void Main(string[] args)
         {
 
-            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
-
-            var orderedWords = words.OrderBy(x => x.Length).Select(x => new {Word = x, Num = x.Length});
-
-            foreach (var word in orderedWords)
+            List<Student> students = new List<Student>
             {
-                Console.WriteLine($"В слове {word.Word} - {word.Num} букв");
+             new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+             new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+              new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+              new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
+
+            var applications = from student in students
+                               let yearOfBirth = DateTime.Now.Year - student.Age
+                               where student.Age < 27
+                               select new Application
+                               {
+                                   Name = student.Name,
+                                   YearOfBirth = yearOfBirth,
+                               };
+
+            foreach (var app in applications)
+            {
+                Console.WriteLine(app.Name + " " + app.YearOfBirth);
             }
         }
 
+    }
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Languages { get; set; }
+    }
 
+    public class Application
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
     }
 }
